a66f99d8f4e9a2bf066ae848c8fdddf863fb24c4
Suyesh Bhandari <suyeshb@gmail.com>
2016-06-20 22:37:28 -0400

    Projects can now be updated.

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -7,12 +7,19 @@ RSpec.feature "Users can edit existing projects" do
     click_link "Sublime Text 3"
     click_link "Edit Project"
   end
-  
+
   scenario "With valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
     click_button "Update Project"
 
     expect(page).to have_content "Project has been updated."
     expect(page).to have_content "Sublime Text 4 beta"
+  end
+
+  scenario "When providing invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content "Project has not been updated."
   end
 end
